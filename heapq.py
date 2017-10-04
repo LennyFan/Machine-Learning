7bbf6b85aa9738aa16a3a25ee8c790a212995972
LennyFan <cfan15@jhu.edu>
2017-10-04 13:08:59 -0400

    Create heapq.py

---
+++ heapq.py
@@ -0,0 +1,24 @@
+inport heapq
+# for all k
+# a[k] <= a[2*k+1] and a[k] <= a[2*k+2] 
+
+q = []
+
+heapq.heappush(q, (5, 'bwa'))
+heapq.heappush(q, (7, 'bwa'))
+heapq.heappush(q, (2, 'bwa'))
+heapq.heappush(q, (1, 'bwee'))
+heapq.heappush(q, (3, 'kiki'))
+
+print q 
+
+while q:
+    next_item = heapq.heappop(q)
+    print q
+    
+> [(1, 'bwee'), (2, 'code'), (3, 'sleep'), (5, 'sleep'), (7, 'sleep')]
+> [(2, 'code'), (3, 'sleep'), (5, 'sleep'), (7, 'sleep')]
+> [(3, 'sleep'), (7, 'sleep'), (5, 'sleep')]
+> [(5, 'sleep'), (7, 'sleep')]
+> [(7, 'sleep')]
+> []
