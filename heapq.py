3392fff3e4ddf5e2f5b6fb2b9b70e6c6a98a6e81
LennyFan <cfan15@jhu.edu>
2017-10-12 15:44:35 -0400

    Update heapq.py

--- heapq.py
+++ heapq.py
@@ -3,30 +3,72 @@ inport heapq
 # a[k] <= a[2*k+1] and a[k] <= a[2*k+2] 
 # we can use this to achieve shortest-path algorithm
 
-q = []
 
+#### Basic Usage
+q = []
 heapq.heappush(q, (5, 'bwa'))
 heapq.heappush(q, (7, 'bwa'))
 heapq.heappush(q, (2, 'bwa'))
 heapq.heappush(q, (1, 'bwee'))
 heapq.heappush(q, (3, 'kiki'))
 
-print q 
-
 while q:
     next_item = heapq.heappop(q)
     print q
-    
 > [(1, 'bwee'), (2, 'code'), (3, 'sleep'), (5, 'sleep'), (7, 'sleep')]
 > [(2, 'code'), (3, 'sleep'), (5, 'sleep'), (7, 'sleep')]
 > [(3, 'sleep'), (7, 'sleep'), (5, 'sleep')]
 > [(5, 'sleep'), (7, 'sleep')]
 > [(7, 'sleep')]
 > []
+# <<< Note that if two entry have same value, heapq will pop by the alphabetical order >>>
+
+#### Q: If the priority of a task changes, how do you move it to a new position in the heap?
+# if key has already in your heapq, then change the key to be "<removed>"
+# use this way to update the heapq and remaint the property
+
+pq = []
+track = {}
+for item in ['hi', 'yo', 'hi', 'yo' ,'yo' , 'yoyoyo','zb','za','a','c','b','l']:
+    if item not in track:
+        track[item] = [1, item]
+        heapq.heappush(pq, track[item])
+    else:
+        track[item][1] = "removed"
+        num,_ = track.pop(item)
+        track[item] = [num +1, item ]
+        heapq.heappush(pq, track[item])
+
+print pq
+> [[1, '<removed>'],
+ [1, '<removed>'],
+ [1, 'l'],
+ [1, 'a'],
+ [1, 'b'],
+ [1, 'yoyoyo'],
+ [1, 'zb'],
+ [2, '<removed>'],
+ [1, 'za'],
+ [3, 'yo'],
+ [1, 'c'],
+ [2, 'hi']]
+
+while pq:
+    a = heapq.heappop(pq)
+    if a[1] != "removed":
+        print a
+> [1, 'a'] #
+[1, 'b']   # if same amount, pop by alphabetical order 
+[1, 'c']   #
+[1, 'l']
+[1, 'yoyoyo']
+[1, 'za']
+[1, 'zb']
+[2, 'hi']
+[3, 'yo']
 
 
-
-# Dijkstra's algorithm
+#### Dijkstra's algorithm
 # adjacent matrix
 def Dijkstra_python(adjacent, source):
     # initial distance
@@ -50,6 +92,7 @@ def Dijkstra_python(adjacent, source):
             elif distance[i] > adjacent[cur_node][i] + cur_distance:
                 distance[i] = adjacent[cur_node][i] + cur_distance
                 # if having better solution, push it into heapq
+                # note that this part I didn't deal with the duplicate node in heapq(q)
                 heapq.heappush(q, (distance[i], i))
     return distance
 
